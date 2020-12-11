6042ac17c9f93b93c1e491dec5b197943c072d58
Austin Lam <lamaustin68@gmail.com>
2020-12-10 17:38:06 -0800

    Initial Commit

---
+++ UCINowClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UCINowClone
+//
+//  Created by Austin Lam on 12/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
